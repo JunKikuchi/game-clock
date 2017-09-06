99522bb6ed62d5f14f61b7dd6c4225c87c02d762
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-06 09:21:15 +0900

    差し切り時計実装

--- src/GameClock/Clock.hs
+++ src/GameClock/Clock.hs
@@ -1,17 +1,22 @@
 module GameClock.Clock
   ( Millisec
   , Clock
+  , wordClock
   , countDown
   , isOver
   ) where
 
 type Millisec = Int
-type Clock = Millisec
+data Clock    = Word Millisec deriving (Show)
+
+wordClock :: Millisec -> Clock
+wordClock = Word
 
 countDown :: Millisec -> Clock -> Clock
-countDown ms c = if c' >= minBound then c' else minBound
+countDown ms (Word c) = Word ms'
   where
-    c' = c - ms
+    c'  = c - ms
+    ms' = if c' >= 0 then c' else 0
 
 isOver :: Clock -> Bool
 isOver = undefined
