47d25d1c7764f8c1ae30f728145c5ccf15dbefe8
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-10 15:40:06 +0900

    Unit.hs に haddock 追加

--- src/GameClock/Clock/Unit.hs
+++ src/GameClock/Clock/Unit.hs
@@ -10,24 +10,30 @@ module GameClock.Clock.Unit
 
 import GameClock.Clock.Sec
 
+-- | 時計ユニットデータ
 data Unit = RoundDown Sec         -- 切り捨て
           | Delay Sec             -- 秒読み
           | TimeLimit Sec         -- 持ち時間
           | ConsiderationTime Sec -- 考慮時間
           deriving (Show, Eq)
 
+-- | 切り捨て時計ユニット作成
 roundDown :: Sec -> Unit
 roundDown = RoundDown
 
+-- | 秒読み時計ユニット作成
 delay :: Sec -> Unit
 delay = Delay
 
+-- | 持ち時間時計ユニット作成
 timeLimit :: Sec -> Unit
 timeLimit = TimeLimit
 
+-- | 考慮時間時計ユニット作成
 considerationTime :: Sec -> Unit
 considerationTime = ConsiderationTime
 
+-- | 秒数カウントダウン
 countDown :: Sec -> Unit -> (Sec, Unit)
 countDown s (RoundDown r) = (s', RoundDown r)
   where s' = (s `div` r) * r
@@ -39,6 +45,7 @@ countDown s (TimeLimit t)
 countDown s (ConsiderationTime c) = (s', ConsiderationTime 0)
   where s' = if s <= c then 0 else (s - c)
 
+-- | 時間切れ判定
 over :: Unit -> Bool
 over (RoundDown _) = True
 over (Delay _) = True
