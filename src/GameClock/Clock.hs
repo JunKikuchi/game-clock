f8a9b4d9aec6f5e5cdd35eb6e1924d7ba162b607
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-10 15:43:45 +0900

    Clock.hs に haddock 追加

--- src/GameClock/Clock.hs
+++ src/GameClock/Clock.hs
@@ -8,13 +8,17 @@ module GameClock.Clock
 import           GameClock.Clock.Sec
 import qualified GameClock.Clock.Unit as Unit
 
+-- | 時計データ
 newtype Clock = Clock [Unit.Unit] deriving (Show)
 
+-- | 時計作成
 clock :: [Unit.Unit] -> Clock
 clock = Clock
 
+-- | 秒数カウントダウン
 countDown :: Sec -> Clock -> Clock
 countDown = undefined
 
+-- | 時間切れ判定
 over :: Clock -> Bool
 over = undefined
