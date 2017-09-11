026e6847e97ead60d370279d66bd2a17092253dc
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-12 00:12:53 +0900

    テストの型宣言追加

--- test/test.hs
+++ test/test.hs
@@ -5,6 +5,7 @@ import qualified GameClockTest.ClockTest as ClockTest
 import qualified GameClockTest.CountDownTest as CountDownTest
 import qualified GameClockTest.OverTest as OverTest
 
+main :: IO ()
 main = defaultMain $ testGroup "GameClock" [UnitTest.tests, ClockTest.tests, tests]
 
 tests :: TestTree
